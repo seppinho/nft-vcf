3e56dbf32a93cd9edfcd9598da77853aa3e27833
Sebastian Schoenherr <sebastian.schoenherr@i-med.ac.at>
2023-11-21 13:39:48 +0100

    Add header method

--- src/main/java/genepi/nf/test/vcf/Methods.java
+++ src/main/java/genepi/nf/test/vcf/Methods.java
@@ -1,9 +1,21 @@
 package genepi.nf.test.vcf;
 
+import java.nio.file.Path;
+
+import htsjdk.variant.vcf.VCFFileReader;
+import htsjdk.variant.vcf.VCFHeader;
+
 public class Methods {
 
 	public static void helloVCF() {
 		System.out.println("Hello VCF");
+	}
+	
+	public static VCFHeader getVcfHeader(Path vcfFilename) {
+		VCFFileReader reader = new VCFFileReader(vcfFilename, false);
+		VCFHeader header = reader.getFileHeader();
+		reader.close();
+		return header;
 	}
 	
 }
